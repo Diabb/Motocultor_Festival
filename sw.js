6971865a4731c5cf226574efb696412f5d60b6fa
Diabb <gwen.chat.11@gmail.com>
2026-06-28 16:58:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'motocultor-v6';
+const CACHE = 'motocultor-v7';
 const ASSETS = ['./index.html', './data.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
